fad13ed954e1a36ce4706873d63a0bc0e02b38da
Gosha1990 <123661130+Gosha1990@users.noreply.github.com>
2023-05-22 20:51:51 +0300

    task36

--- HM_7/task_36.py
+++ HM_7/task_36.py
@@ -7,12 +7,11 @@
 # Ввод: print_operation_table(lambda x, y: x * y)
 
 
-
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    for i in range(1,num_rows +1):
+    for i in range(1, num_rows + 1):
         print()
-        for j in range(1,num_columns+1):
-            print(operation(i,j), end=" ")
-print_operation_table(lambda x,y: x*y,5,5)
+        for j in range(1, num_columns + 1):
+            print(operation(i, j), end=" ")
 
 
+print_operation_table(lambda x, y: x * y, 5, 5)
