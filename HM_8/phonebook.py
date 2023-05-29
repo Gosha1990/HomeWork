8a0a86d83edad6bcaf19de3efbd23301b75261e3
Gosha1990 <123661130+Gosha1990@users.noreply.github.com>
2023-05-29 18:29:42 +0300

    функции на ввод имени

--- HM_8/phonebook.py
+++ HM_8/phonebook.py
@@ -7,7 +7,7 @@
 # 7. Удалить контакт
 # 8. Выход
 
-# path = 'contact.txt'
+path = 'contact.txt'
 # data = open(path, 'w')
 # for line in data:
 #     print(line)
@@ -15,7 +15,7 @@
 
 def open_file_read(path):
     '''Функуия считыает данные из файла тхт показывает пользователю'''
-    with open(path,'r') as file:
+    with open(path,'r', encoding='UTF-8') as file:
         main_list=(file.readlines())
         main_list_str=[x.rstrip().split(':') for x in main_list]
     return main_list_str
@@ -24,7 +24,7 @@ print(open_file_read('contact.txt'))
 
 def open_file_write(path, list_file):
     '''Функция вносит изменения в файл тхт'''
-    with open(path, 'w') as file:
+    with open(path, 'w', encoding='UTF-8') as file:
         file.writelines([':'.join(x)+'\n' for x in list_file])
 # list_for_look=[['Prohor', '89234567436', 'coment'], ['Boris', '89234542312', 'coment'], ['Klava', '89834535445', 'Coment'], ['sadrgf', 'sdfg', 'dfg'], ['sgdsf', 'sdfg', 'sgdf']]
 def look_list (list_for_look):
@@ -33,22 +33,36 @@ def look_list (list_for_look):
 # look_list(list_for_look)
 
 
-def interaction():
+def interaction()->int:
     '''Выбор действия пользователя'''
-    action = int(input('Введите команду: \n1 - Сделать запись\n2 - Получить контакт\n - Удалить запись'))
+    action = int(input('Введите команду: \n1 - Сделать запись\n2 - Получить контакт\n3 - Удалить запись'))
     if action == 1:
         contact = input('Вводите ФИО и номер через пробел: ')
         return (contact, action)
-    if action == 2:
+    elif action == 2:
         contact = input('Вводим имя контакта: ')
         return (contact, action)
-    if action == 3:
+    elif action == 3:
         contact = input('Вводим контакт для удаления: ')
         return (contact, action)
 
 
 
-# print(interaction(open_file_read('contact.txt')))
+def GetUser():
+    sname = input("Введите Фамилию: " + "\n") + " "
+    name = input("Введите Имя: " + "\n") + " "
+    fname = input("Введите Отчество: " + "\n") + " "
+    number = input("Введите номер: " + "\n") + " "
+    return (sname + name + fname + number)
+
+def EditBase():
+    print("_________________________________________________")
+    open_file_write(GetUser())
+    print("_________________________________________________")
+
+
+
+
 
 
 
